7b4d3f7fd0c7967bd245807cc2a6dae90f16855e
Jesse James <fitforgov@gmail.com>
2025-04-14 18:21:09 -0700

    Update next.config.js to allow SVG images

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
